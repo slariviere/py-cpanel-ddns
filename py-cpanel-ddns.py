db0d1461f6f4be44fea6cbd33f9ca7afc24ef435
Sébastien Larivière <sebastien@lariviere.me>
2014-05-13 18:41:28 -0400

    Update cpanel record

--- py-cpanel-ddns.py
+++ py-cpanel-ddns.py
@@ -13,6 +13,7 @@ configMap = yaml.safe_load(configFile)
 configFile.close()
 
 # Connect to the server
+print "[+] Connecting to cpanel server (" + configMap['cpanel']['hostname'] + ")"
 server =  pycpanel.conn(configMap['cpanel']['hostname'], configMap['cpanel']['username'], cpanel_hash, password=None, ssl=True, verify=True, check_conn=True)
 
 # Params for the first connection (Get the line number to edit)
@@ -20,14 +21,31 @@ params = { 'domain' : configMap['ddns']['domain'] }
 
 dumpzone = server.api('dumpzone', params=params)
 
+found = False
 for line in dumpzone['result'][0]['record']:
     if 'name' in line:
         if line['name'] == configMap['ddns']['name']:
+            found = True
             break
 
+if not found:
+    print "Requested record not found"
+    exit(1)
+
+# Get the WAN ip address
 p = urllib2.urlopen(configMap['ip']['url'])
 ip = p.read();
-print ip
-print line
+
+# Prepare the edit parameter
+editParams = {
+        'domain': configMap['ddns']['domain'],
+        'line': line['Line'],
+        'address': ip,
+        'type': 'A'
+}
 
 
+editzone = server.api('editzonerecord', params=editParams)
+
+if "Bind reloading on" in  editzone['result'][0]['statusmsg']:
+    print "[+] Update successful:" + editzone['result'][0]['statusmsg']
